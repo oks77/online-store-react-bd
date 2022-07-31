876a406c29adb5899ea08cf8a8808f13a2885b73
Oksana Stavenko <oksanastavenko@gmail.com>
2022-07-31 13:58:25 +0300

    first commit

---
+++ precache-manifest.9f043b8929b096fccca052da13447623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08212dda7cafe11eeabc",
+    "url": "https://oks77.github.io/online-store-react/static/css/main.28338edb.chunk.css"
+  },
+  {
+    "revision": "08212dda7cafe11eeabc",
+    "url": "https://oks77.github.io/online-store-react/static/js/main.08212dda.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "https://oks77.github.io/online-store-react/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "199af2c2a0c941a3b7e7",
+    "url": "https://oks77.github.io/online-store-react/static/js/2.199af2c2.chunk.js"
+  },
+  {
+    "revision": "5264ffeac50301a1294f",
+    "url": "https://oks77.github.io/online-store-react/static/js/runtime~main.5264ffea.js"
+  },
+  {
+    "revision": "c0e35b3e6d6dbb24dc34b3d618eb29bf",
+    "url": "https://oks77.github.io/online-store-react/index.html"
+  }
+];
